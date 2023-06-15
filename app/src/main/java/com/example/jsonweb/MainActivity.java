43445897b47024745aa5e4851fd8044a9155cc0f
dquinteroa1 <dquinteroa@uteq.edu.ec>
2023-06-15 17:24:15 -0500

    recibicir tokens

--- app/src/main/java/com/example/jsonweb/MainActivity.java
+++ app/src/main/java/com/example/jsonweb/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,12 +28,12 @@ import WebService.Asynchtask;
 import WebService.WebService;
 
 public class MainActivity extends AppCompatActivity implements Asynchtask {
-    private TextView txtS;
+    //private TextView txtS = (TextView)findViewById(R.id.txtToken);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtS = (TextView)findViewById(R.id.txtHolla);
+
     //PIRMER LIBRERIA (AUTH: ING. CRISTIAN )
     /*
         Bundle bundle = this.getIntent().getExtras();
@@ -63,40 +66,77 @@ public class MainActivity extends AppCompatActivity implements Asynchtask {
     */
 
         // Consumir API de KUSKHI COn LIBRERIA DEL ING XD
-
-
-
+        /*
         Map<String, String> datos = new HashMap<String, String>();
         WebService ws= new
                 WebService("https://api-uat.kushkipagos.com/transfer-subscriptions/v1/bankList",
                 datos, MainActivity.this, MainActivity.this);
         ws.execute("GET","Public-Merchant-Id","84e1d0de1fbf437e9779fd6a52a9ca18");
+         */
 
     }
     @Override
     public void processFinish(String result) throws
             JSONException {
         //txtSaludo.setText("Resp: " + result );
-         //txtS = (TextView)findViewById(R.id.txtHolla);
-        //txtS.setText(result);
+        TextView txtS = (TextView)findViewById(R.id.txtToken);
+         String tok = "";
+
+        Log.i("Resp: ", result);
+        JSONObject resp = new JSONObject(result);
+        if(resp.has("error")){
+            Toast.makeText(this, "Error Login" + resp.getString("error"), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "Hola"+ resp.getString("access_token"), Toast.LENGTH_SHORT).show();
+            txtS.setText("Token: " + resp.getString("access_token"));
+            tok=  resp.getString("access_token");
+            //ENviar el token a la otra actividad para enlistar los productos
+
+            Intent i = new Intent(this, MainActivity2.class);
+            i.putExtra("Token",tok);
+            startActivity(i);
+
+        }
+
+
 
 
         //Parsear un JSON
+        /*
         String lstBancos="";
         JSONArray JSONlista = new JSONArray(result);
         for(int i=0; i< JSONlista.length();i++){
             JSONObject banco= JSONlista.getJSONObject(i);
-            lstBancos = lstBancos + "\n" +
-                    banco.getString("name").toString();
+            lstBancos = banco.getString("access_token").toString();
         }
-        txtS.setText("Respuesta WS Lista de Bancos" + lstBancos);
+        txtS.setText("Token: " + lstBancos);
+
+         */
+
+
+
     }
-    public void Enviar(View view){
+    public void Enviar(View view) {
         //Se crea una instancia de la actividad que se quiere abrir
-        Intent i = new Intent(this, MainActivity2.class);
+        //Intent i = new Intent(this, MainActivity2.class);
         //se envia el dato a la siguiente actividad
         //i.putExtra("cedula",txtCedula.getText().toString());
         //se inicia la actividad
-        startActivity(i);
+        //
+
+        EditText txtUser = (EditText)findViewById(R.id.txtCedula);
+        EditText txtContra = (EditText)findViewById(R.id.txtNombres);
+
+
+        //Recibir el token y verlo en el txt
+        Map<String, String> datos = new HashMap<String, String>();
+        datos.put("correo", txtUser.getText().toString());
+        datos.put("clave", txtContra.getText().toString());
+        WebService ws = new WebService(
+                "https://api.uealecpeterson.net/public/login",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("POST");
+
     }
 }
--- app/src/main/java/com/example/jsonweb/MainActivity2.java
+++ app/src/main/java/com/example/jsonweb/MainActivity2.java
@@ -14,21 +14,32 @@ import WebService.Asynchtask;
 import WebService.WebService;
 
 public class MainActivity2 extends AppCompatActivity implements Asynchtask {
-    private TextView txtList = (TextView)findViewById(R.id.txtList) ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+
+        String token = getIntent().getStringExtra("Token");
+        //lbllista.setText(token);
+
+
+
+
         Map<String, String> datos = new HashMap<String, String>();
+        datos.put("fuente","1");
+        datos.put("Authorization","Bearer"+token);
         WebService ws= new
-                WebService("https://api-uat.kushkipagos.com/transfer-subscriptions/v1/bankList",
+                WebService("https://api.uealecpeterson.net/public/productos/search",
                 datos, MainActivity2.this, MainActivity2.this);
-        ws.execute("GET","Public-Merchant-Id","84e1d0de1fbf437e9779fd6a52a9ca18");
+        ws.execute("POST","access_token",token);
+
+
     }
 
     @Override
     public void processFinish(String result) throws JSONException {
-        txtList.setText(result);
+        TextView lbllista = (TextView)findViewById(R.id.txtList);
+        lbllista.setText(result);
     }
 }
